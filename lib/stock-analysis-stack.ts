4ad8ca2e195e82a72c16d2ff7e1452231b6c0158
soruma <soruma.net@gmail.com>
2024-12-01 18:24:30 +0900

    Rename from download-stocks to download-listed-info

--- lib/stock-analysis-stack.ts
+++ lib/stock-analysis-stack.ts
@@ -22,35 +22,45 @@ export class StockAnalysisStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props: StockAnalysisLambdaStackProps) {
         super(scope, id, props);
 
-        /***         * S3         */
+        /***
+         * S3
+         */
         const s3 = new Bucket(this, props.resourceName.s3Name(), {
             bucketName: props.resourceName.s3Name(),
         });
 
-        /***         * Lambda         */
-        const downloadStocksFunction = new NodejsFunction(this, props.resourceName.lambdaName('download-stocks'), {
-            entry: 'assets/lambdas/download-stocks/index.ts',
-            handler: 'handler',
-            architecture: Architecture.ARM_64,
-            runtime: Runtime.NODEJS_22_X,
-            timeout: cdk.Duration.seconds(30),
-            environment: {
-                JQUANTS_API_MAIL_ADDRESS: process.env.JQUANTS_API_MAIL_ADDRESS || '',
-                JQUANTS_API_PASSWORD: process.env.JQUANTS_API_PASSWORD || '',
-                S3_BUCKET_NAME: s3.bucketName,
+        /***
+         * Lambda
+         */
+        const downloadListedInfoFunction = new NodejsFunction(
+            this,
+            props.resourceName.lambdaName('download-listed-info'),
+            {
+                entry: 'assets/lambdas/download-listed-info/index.ts',
+                handler: 'handler',
+                architecture: Architecture.ARM_64,
+                runtime: Runtime.NODEJS_22_X,
+                timeout: cdk.Duration.seconds(30),
+                environment: {
+                    JQUANTS_API_MAIL_ADDRESS: process.env.JQUANTS_API_MAIL_ADDRESS || '',
+                    JQUANTS_API_PASSWORD: process.env.JQUANTS_API_PASSWORD || '',
+                    S3_BUCKET_NAME: s3.bucketName,
+                },
+                logRetention: RetentionDays.THIRTEEN_MONTHS,
             },
-            logRetention: RetentionDays.THIRTEEN_MONTHS,
-        });
+        );
 
-        s3.grantPut(downloadStocksFunction);
+        s3.grantPut(downloadListedInfoFunction);
 
-        /***         * EventBridge         */
+        /***
+         * EventBridge
+         */
         const rule = new Rule(this, 'DailyRule', {
             schedule: Schedule.cron({ minute: '0', hour: '15' }),
         });
 
         rule.addTarget(
-            new LambdaFunction(downloadStocksFunction, {
+            new LambdaFunction(downloadListedInfoFunction, {
                 event: RuleTargetInput.fromObject({
                     message: 'Scheduled Event',
                 }),
